41e735a55aad54c5c495223d80cddc44d3138b8b
TheEnumbin <enumbin@gmail.com>
2024-11-16 23:22:09 +0600

    Ready to release 1.2.3.1

--- infofields.php
+++ infofields.php
@@ -40,7 +40,7 @@ class Infofields extends Module
     public function __construct()
     {
         $this->name = 'infofields';
-        $this->version = '1.2.3';
+        $this->version = '1.2.3.1';
         $this->tab = 'pricing_promotion';
         $this->author = 'TheEnumbin';
         $this->need_instance = 0;
