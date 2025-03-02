d1f0a43c8ee2f934bcbc9323f028e3760ad7eef1
TheEnumbin <enumbin@gmail.com>
2025-03-02 23:58:37 +0600

    working on meta import

--- includes/InfofieldDb.php
+++ includes/InfofieldDb.php
@@ -94,4 +94,51 @@ class InfofieldDB
 
         return false;
     }
+
+    public function insert_infofields_meta($row)
+    {
+        $query = '
+            INSERT INTO `' . _DB_PREFIX_ . 'infofields_meta` (
+            `id_infofields`, `parent_item_id`
+            ) VALUES (
+            ' . pSQL($row[2]) . ',
+            ' . pSQL($row[3]) . ',
+            ' . (empty($row[4]) ? 'NULL' : "'" . pSQL($row[4]) . "'") . ',
+            ' . (empty($row[5]) ? 'NULL' : "'" . pSQL($row[5]) . "'") . ',
+            ' . (strtoupper($row[6]) === 'TRUE' ? 1 : 0) . ',
+            ' . (strtoupper($row[7]) === 'TRUE' ? 1 : 0) . ',
+            ' . (empty($row[8]) ? 'NULL' : (int) $row[8]) . ',
+            ' . (empty($row[9]) ? 'NULL' : (int) $row[9]) . '
+            )';
+
+        if (Db::getInstance()->execute($query)) {
+            return Db::getInstance()->Insert_ID();
+        }
+
+        return false;
+    }
+
+    public function insert_infofields_meta_lang($row, $inf_meta_id)
+    {
+        if (empty($row[0])) {
+            return false;
+        }
+        $lang_id = Context::getContext()->language->id;
+        $query = '
+            INSERT INTO `' . _DB_PREFIX_ . 'infofields_meta_lang` (
+            `id_infofields_meta`, `id_lang`, `meta_data`
+            ) VALUES (
+            ' . pSQL($inf_meta_id) . ',
+            ' . pSQL($lang_id) . ',
+            "' . pSQL($row[0]). '",
+            ' . (empty($row[1]) ? "''" : "'" . pSQL($row[1]) . "'") . ',
+            ' . (empty($row[4]) ? "''" : "'" . pSQL($row[4]) . "'") . '
+            )';
+
+        if (Db::getInstance()->execute($query)) {
+            return true;
+        }
+
+        return false;
+    }
 }
